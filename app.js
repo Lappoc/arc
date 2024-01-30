13a7bb23833c4eb63a052fe767aa1d4d7988adc8
Lappoc <71691965+Lappoc@users.noreply.github.com>
2024-01-30 13:05:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,47 +8,33 @@
 //     var audio = new Audio('starboy.mp3');
 //     audio.play();
 //   }
- 	// Engine constants
      const ENGINE_DEBUG = true;
 	
-     // Engine vaiables
-     var canvas = null; 	  // canvas DOM object
-     var context = null;   // canvas context
-     var frameCount = 0;   // Frame count 
-     var fps = 0; 		  // Frames per Second
+     var canvas = null; 	  
+     var context = null;  
+     var frameCount = 0;   
+     var fps = 0; 		  
  
-     
-     
-     /* HAGE CORE
-     -----------------------------------------*/
-     
-     // HAGE Run
      function hageRun() {
          canvas=document.getElementById("canvas");
          context=canvas.getContext("2d");			
-         init(); 	// Init game scene	
-         draw(); 	// Draw game scene					
-         hageLoop();	// Start game loop
-         hageFps();	// Start FPS Counter
+         init(); 
+         draw(); 				
+         hageLoop();	
+         hageFps();	
      }
      
-     // HAGE Default. Set default engine parameters
      function hageDefault() {
-         // Set default text font
          textFont('12pt Arial');
-         // Set default text color
          textColor('#999');
-         // Set default color
          borderColor('#fff');
      }
      
-     // HAGE Clear
      function hageClear() {
          context.clearRect(0,0,canvas.width, canvas.height);
          context.beginPath();
      }
      
-     // HAGE main loop
      function hageLoop() {
          var start = new Date().getTime(),
          time = 0;
@@ -56,16 +42,15 @@
              time += 15;
              var diff = (new Date().getTime() - start) - time;			
              hageClear();
-             hageDefault(); // Setup HAGE default paramaters
-             update(); 	   // Update game scene
-             draw(); 	   // Draw game scene			 
+             hageDefault(); 
+             update(); 	 
+             draw(); 	 		 
              frameCount++;					
              window.setTimeout(timer, (15 - diff));			
          }	
          window.setTimeout(timer, 15);		
      }
      
-     // HAGE FPS Counter
      function hageFps() {
          var start = new Date().getTime(),
          time = 0;
@@ -78,71 +63,50 @@
          window.setTimeout(instance, 1000);			
      }
  
-     // Set FPS
      function setFPS() {
          fps = frameCount;
          frameCount = 0;		
      }
-     
-     
-     /* HAGE SCENE
-     -----------------------------------------*/	
- 
-     // Set canvas background color
+
      function background(color) {
          canvas = document.getElementById('canvas');
          canvas.style.background = color;
      }	
  
-     // Hide cursor
      function noCursor() {
          canvas = document.getElementById('canvas');
          canvas.style.cursor = "url('data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='), auto";
      }	
  
-     // Show default cursor  
      function defaultCursor() {
          canvas = document.getElementById('canvas');
          canvas.style.cursor = "default";
      }	
  
-     // Show custom cursor
      function customCursor(filename) {
          canvas = document.getElementById('canvas');
          canvas.style.cursor = "url('"+filename+"'),auto";
      }
      
-     
-     /* HAGE TYPOGRAPHY
-     -----------------------------------------*/	
-     
-     // Set text font	
+
      function textFont(font) {
          context.font = font;
      }	
      
-     // Set text color
      function textColor(color) {
          context.fillStyle = color;
      }
      
-     // Draw text
      function text(str, x, y) {
          context.fillText(str, x, y);
      }
      
-     
-     /* HAGE INPUT
-     -----------------------------------------*/	
-     
-     // Mouse input
      var mouseX = 0;
      var mouseY = 0;
      var mouseClickX = 0;
      var mouseClickY = 0;
      var mouseClick = false;
      
-     // Keyboard input
      var KEY = 0;
      var KEY_LEFT  = false;
      var KEY_RIGHT = false;
@@ -156,29 +120,24 @@
      var	KEY_K 	  = false;
      var	KEY_M     = false;
      
-     // Get mouse X and Y
      function hageMouseMove(event) {
          mouseX = event.clientX - canvas.offsetLeft;    
          mouseY = event.clientY - canvas.offsetTop;    
      }
  
-     // Get mouse click X and Y
      function hageMouseClick(event) {
          mouseClickX = event.clientX - canvas.offsetLeft;    
          mouseClickY = event.clientY - canvas.offsetTop;    		
      }
          
-     // Set mouse click true
      function hageMouseDown(event) {			
          mouseClick = true;		
      }
      
-     // Set mouse click false
      function hageMouseUp(event) {		
          mouseClick = false;		
      }	 
      
-     // Get keyboard press buttons
      function hageKeyboardUpdateStates(event) {				
          KEY = event.keyCode;		
          if(KEY == 39) KEY_RIGHT = true; 
@@ -196,7 +155,6 @@
          if(ENGINE_DEBUG) console.log(KEY);
      }
      
-     // Clear keybord states
      function hageKeyboardClearStates(event) {
          KEY = event.keyCode;
          KEY_RIGHT = false;
@@ -212,57 +170,42 @@
          KEY_SPACE = false;		
      }
      
-     
-     
-     /* HAGE PRIMITIVES
-     -----------------------------------------*/	
-     
-     // Set border color
      function borderColor(color) {
          context.strokeStyle = color;
      }
      
-     // Set line width
      function lineWidth(width) {	
          context.lineWidth = width;		    
      }
      
-     // Set line style: butt, round, or square.                
      function lineStyle(style) {		
          context.lineCap = cap; 
      }	
      
-     // Begin path
      function hageBegin() {
          context.beginPath();
      }
      
-     // Move to
      function moveTo(x, y) {
          context.moveTo(x, y);
      }
      
-     // Line to
      function lineTo(x, y) {
          context.lineTo(x, y);
      }	
      
-     // Create a Quadratic curves
      function quadraticCurveTo(controlX, controlY, endingPointX, endingPointY) {
          context.quadraticCurveTo(controlX, controlY, endingPointX, endingPointY);
      } 
      
-     // Create a Bezier curve
      function bezierCurveTo(cPointX1, cPointY1, cPointX2, cPointY2, endPointX, endPointY) {
          context.bezierCurveTo(cPointX1, cPointY1, cPointX2, cPointY2, endPointX, endPointY); 
      }
      
-     // End path
      function hageEnd() {
          context.stroke();
      }	
-     
-     // Draw circle
+
      function circle(x, y, radius, fill_style) {
          startingAngle =  0 * Math.PI;
          endingAngle = 2 * Math.PI;
@@ -273,7 +216,6 @@
          context.stroke(); 
      } 
      
-     // Draw line
      function line(x1, y1, x2, y2, cap) {                            
          context.beginPath();           
          context.moveTo(x1,y1);
@@ -281,7 +223,6 @@
          context.stroke();
      }
      
-     // Draw arc
      function arc(x, y, radius, startAngle, endAngle) {
          startingAngle = startAngle * Math.PI;
          endingAngle = endAngle * Math.PI;
@@ -290,7 +231,6 @@
          context.stroke();    
      }
      
-     // Draw curve
      function curve(x, y, controlX, controlY, endingPointX, endingPointY) {
          context.beginPath();              
          context.moveTo(x, y);
@@ -298,7 +238,6 @@
          context.stroke();
      }    
      
-     // Draw bezier
      function bezier(x, y, cPointX1, cPointY1, cPointX2, cPointY2, endPointX, endPointY) {
          context.beginPath();
          context.moveTo(x, y);
@@ -306,7 +245,6 @@
          context.stroke();
      }
      
-     // Draw rectangle
      function rectangle(topLeftCornerX, topLeftCornerY, width, height, fill_style) {
          context.beginPath();
          context.rect(topLeftCornerX, topLeftCornerY, width, height);
@@ -314,44 +252,33 @@
          context.fill();
          context.stroke();
      }   	
- 
- 
- 
-         
+  
          var game_screen_width  = 640;  
-         var game_screen_height = 460; // canvas 480
+         var game_screen_height = 460; 
      
-         // Game stages
          const menu_stage  = 0;
          const game_stage  = 1;		
          const score_stage = 2;
          
-         
          var game_start = false;
-         
          
          var game_current_stage = menu_stage;
          
-         // Game control
          const mouse_control    = 0;
          const keyboard_control = 1;
          
          var game_current_control = mouse_control;
          
-         // Game objects
          var ball = new Object();
          var paddle = new Object();			
          var bricks = new Object();
          
-         // Game level
          var level = 1;
          var score = 0;
          var lives = 3;
          var speed = 5;
          
- 
-         // Bricks
-         var bricks_destroed = 0;
+        var bricks_destroed = 0;
          var bricks_count = 0;
 				
          var level_bricks_count1  = 84;
@@ -393,8 +320,6 @@
                               [1,1,1,1,1,1,2,1,1,1,1,1,1,1]];
                               
          
- 
-         // Start first level
          var level_bricks_count = level_bricks_count1;
          var level_bricks = level_bricks1;
          
@@ -440,12 +365,6 @@
             textColor("#999");
             text('ouse',417,330);
             textColor("#999");
-             
-            //  textFont('10pt Arial');
-            //  textColor("#333");
-            //  text('Browsers:',20,470);
-            //  text('chrome,',85,470);
-            //  text('safari',135,470);
          }
      
          function initBall() {
@@ -461,19 +380,13 @@
          }
          
          function updateBall() {
-         
-             
-             // Collisions with game level
              if(ball.x + ball.dx + ball.radius > canvas.width || ball.x + ball.dx - ball.radius < 0)  ball.dx = -ball.dx;     
  
-             // Collissions with bricks
              rowheight = bricks.height + bricks.padding;
              colwidth = bricks.width + bricks.padding;
              row = Math.floor(ball.y/rowheight);
              col = Math.floor(ball.x/colwidth);
              
-             // @todo some time collissions is not true!
-             // Ball collision with brick
              if (ball.y < bricks.rows * rowheight && row >= 0 && col >= 0 && level_bricks[row][col] == 1) {
                  ball.dy = -ball.dy;
                  level_bricks[row][col] = 0;
@@ -481,36 +394,26 @@
                  bricks_destroed++; 
              }
              
-             // Ball collision with rock
              if (ball.y < bricks.rows * rowheight && row >= 0 && col >= 0 && level_bricks[row][col] == 2) {
                  ball.dy = -ball.dy;            
                  level_bricks[row][col] = 1;
              }
              
              
-             // Ball collision
              if(ball.y + ball.dy - ball.radius < 20) {
-                 // Collision with top of the room
                  ball.dy = -ball.dy;
              } else {
-                 // Collision with paddle
                  if(ball.y + ball.dy + ball.radius > canvas.height - paddle.height) {
-                     // Collision with paddle
                      if(ball.x + ball.radius > paddle.x && ball.x - ball.radius < paddle.x + paddle.width) {
                          ball.dy = -ball.dy;
                          ball.dx = 8 * ((ball.x-(paddle.x+paddle.width/2))/paddle.width);						
                      } else {                
-                         // Collision with the bottom of the room - Water
                          if(ball.y + ball.dy + ball.radius > canvas.height) {
-                             // Sub lives 
                              lives -= 1;
-                             // Sub score
                              score -= 100;
                              
-                             // Magnet ball
                              game_start = false;
                              
-                             // Respawn ball
                              initBall();
                              
                                          
@@ -519,7 +422,6 @@
                  }
              }
              
-             // Ball moves    
              ball.x += ball.dx;
              ball.y += ball.dy;    
          }
@@ -544,10 +446,8 @@
          function updatePaddle() {
              
              if(game_current_control == mouse_control) {
-                 // Mouse control
                  paddle.x = mouseX - (paddle.width/2);
              } else {
-                 // Keybord control
                  if(KEY_LEFT) paddle.x = paddle.x - 10;
                  if(KEY_RIGHT) paddle.x = paddle.x + 10;
              }
@@ -570,13 +470,11 @@
          function drawBricks() {	
              for (i=0; i < bricks.rows; i++) {
                  for (j=0; j < bricks.cols; j++) {          
-                     // Brick
                      if (level_bricks[i][j] == 1) {         			
                            rectangle((j * (bricks.width + bricks.padding)) + bricks.padding, 
                                          (i * (bricks.height + bricks.padding)) + bricks.padding,
                                                bricks.width, bricks.height,"#fff");                
                      }
-                     // Rock
                      if (level_bricks[i][j] == 2) {                
                          rectangle((j * (bricks.width + bricks.padding)) + bricks.padding, 
                                          (i * (bricks.height + bricks.padding)) + bricks.padding,
@@ -587,14 +485,10 @@
          }
  
          function updateBricks() {
-             
-             // If lives is 0 then game over
              if(lives <= 0) {
                  game_current_stage = score_stage;
              }
              
-             
-             // If bricks destroed for this level then level + 1
              if(bricks_count != 0) {
                  if(bricks_destroed == bricks_count)  {
                      bricks_destroed = 0;
@@ -605,7 +499,6 @@
                  }
              }
              
-             // Load levels
              switch(level) {
                  case 1: 
                      level_bricks_count = level_bricks_count1;
@@ -625,7 +518,6 @@
                  game_current_stage = score_stage;
              }	
  
-             // Init new level here
              initBricks();
              
          }
@@ -672,7 +564,6 @@
          }
          
          function updateScore() {
-             // Its simple works in all browsers :)
              // if(F5) RESTART THE GAME
          }
          
@@ -709,7 +600,6 @@
          
          function draw() {
              textFont("10pt Arial");
-            //  text('FPS: ' + fps, 10, 20);
              
              switch(game_current_stage) {
                  case menu_stage:  drawMenu(); break;
